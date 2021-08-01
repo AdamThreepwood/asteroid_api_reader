738fad13392937673a1cbc275b1aa5167eec53e7
AdamThreepwood <87753284+AdamThreepwood@users.noreply.github.com>
2021-08-01 19:41:59 +0200

    Update fetchData.js
    Removed single quote on API url to avoid recurring calls during editing phase.

--- fetchData.js
+++ fetchData.js
@@ -9,7 +9,7 @@ searchButton.addEventListener("click", ()=>{
 
 
 async function sendApiRequest(){
-  let response = await fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-07-23&end_date=2021-07-30&api_key=DEMO_KEY');
+  let response = await fetch(https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-07-23&end_date=2021-07-30&api_key=DEMO_KEY');
   console.log(response)
   let data = await response.json();
   console.log(data);
