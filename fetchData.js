e39f859569f79eb5a36de7b8ac0a1c45b5e223fd
AdamThreepwood <87753284+AdamThreepwood@users.noreply.github.com>
2021-08-03 17:24:32 +0200

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -1,24 +1,58 @@
+
+let today = new Date().toISOString().slice(0, 10)
+
+
+console.log(today)
+
+function createDate(days, months, years) {
+        var today = new Date();
+        today.setDate(today.getDate() + days);
+        today.setMonth(today.getMonth() + months);
+        today.setFullYear(today.getFullYear() + years);
+        return today;    
+    }
+
+let lastWeek = createDate(-6, 0, 0).toISOString().slice(0, 10)
+
+console.log(lastWeek)
+
+let url = https://api.nasa.gov/neo/rest/v1/feed?start_date=' + lastWeek + '&end_date=' + today + '&api_key=DEMO_KEY'
+
+
+
+
+
+
+
 let searchButton = document.querySelector("#search")
 
 
 searchButton.addEventListener("click", ()=>{
   console.log("button pressed")
-  sendApiRequest()
+  getData()
 })
 
 
 
-async function sendApiRequest(){
-  let response = await fetch(https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-07-23&end_date=2021-07-30&api_key=DEMO_KEY');
-  console.log(response)
-  let data = await response.json();
-  console.log(data);
-  asteroidData(data);
+async function getData(){
+  fetch(url)
+    .then(res => res.json()) 
+    .then(data => {
+
+    
+      console.log(data)
+      
+      
+      
+    })
+
 }
 
 
 
 function asteroidData(data){
-  document.querySelector("#content").innerHTML = data.near_earth_objects["2021-07-30"][0].estimated_diameter.kilometers.estimated_diameter_max;
+  console.log(data)
+  //document.querySelector("#content").innerHTML = data.near_earth_objects["2021-07-30"][0].estimated_diameter.kilometers.estimated_diameter_max;
 
 }
+
